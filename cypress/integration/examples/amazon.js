abcb387112792ac6c18adecb233a921b1405a3f4
Maxim Svera <sveramaxim@gmail.com>
2021-08-19 16:37:53 -0500

    Title  check

--- cypress/integration/examples/amazon.js
+++ cypress/integration/examples/amazon.js
@@ -2,6 +2,7 @@ describe('Amazon Tests', function(){
     it('Verify Title of the landing Page',function(){
         cy.visit('https://www.ebay.com');
        cy.title().should('eq','Electronics, Cars, Fashion, Collectibles & More | eBay')
+       cy.title().should('eq','Electronics, Cars, Fashion, Collectibles & More | eBay')
     })
 
 
